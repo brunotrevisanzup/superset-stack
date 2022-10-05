90d20f088198750f8f4637046bcc26281368a328
brunotrevisanzup <bruno.trevisan@zup.com.br>
2022-10-05 15:23:25 -0300

    alteracao 003

--- superset-app/templates/run_superset.bat
+++ superset-app/templates/run_superset.bat
@@ -1,17 +1,17 @@
 
-#------------------------------##
+REM #------------------------------##
 docker pull apache/superset
-##------------------------------##
-#local:
+REM ##------------------------------##
+REM#local:
 docker run -d -p 8180:8088 --name superset apache/superset
-##------------------------------##
+REM##------------------------------##
 docker exec -it superset superset fab create-admin \
                --username "{{user_email}}" \
                --firstname Admin \
                --lastname Admin \
                --email "{{user_email}}" \
                --password "{{user_pass}}"
-##------------------------------##
+REM##------------------------------##
 docker exec -it superset superset db upgrade
 docker exec -it superset superset load_examples
 docker exec -it superset superset init
